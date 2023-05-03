9b10a0adefbe06530179696f52ca47cd43fc17ed
BetikuOluwatobi <anthonytobynho@gmail.com>
2023-05-03 15:31:21 +0200

    Modifying Forms Field

--- temp_forecast/lstm/forms.py
+++ temp_forecast/lstm/forms.py
@@ -4,7 +4,7 @@ import numpy as np
 class ForecastForm(forms.Form):
   month = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your month...'}))
   day_of_month = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your day of the month...'}))
-  dew_point = forms.FloatField(label='Dew Point Temp (°C)',max_value=10000, min_value=0,widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your dew point...'}))
+  dew_point = forms.FloatField(label='Dew Point Temp (°C)',max_value=10000, min_value=-9999999,widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your dew point...'}))
   relative_humidity = forms.FloatField(label='Rel Hum (%)',max_value=10000, min_value=0,widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your relative humidity...'}))
   wind_speed = forms.FloatField(label='Wind Spd (km/h)',max_value=10000, min_value=0,widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your wind speed...'}))
   stn_press = forms.FloatField(label='Stn Press (kPa)',max_value=10000, min_value=0,widget=forms.NumberInput(attrs={'class': 'form-control','id': "formGroupExampleInput",'placeholder':'Enter your Stn Pressure...'}))
